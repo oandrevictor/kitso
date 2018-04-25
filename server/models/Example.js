8e452d08cb41ce230ddd55ac8603f79c885a300b
gabrielmla <gabmla19@gmail.com>
2018-04-25 15:03:06 -0300

    Back-end structure and example files

---
+++ server/models/Example.js
@@ -0,0 +1,10 @@
+var mongoose = require('mongoose');
+var Schema = mongoose.Schema;
+
+var ExampleSchema = new Schema({
+    nome: String
+});
+
+var Example = mongoose.model('Example', ExampleSchema);
+
+module.exports = Example;
