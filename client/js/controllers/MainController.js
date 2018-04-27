cbe845d9256a542c08966058d049a5e528e445dc
André Lopes <oandrevictor@gmail.com>
2018-04-26 23:52:04 -0300

    Heroku deployment + scss usage

--- client/js/controllers/MainController.js
+++ client/js/controllers/MainController.js
@@ -1,5 +1,10 @@
 angular.module('MainController', []).controller('MainController', function($scope) {
 
-    $scope.tagline = 'You watch, we save it!';   
+    $scope.tagline = 'You watch, we save it!';
+
+    $scope.method = function(){
+      console.log("Method example");
+      console.log("log example");
+    }
 
 });
---
+++ server/config/db.js
@@ -0,0 +1,5 @@
+// config/db.js
+    module.exports = {
+        url : 'mongodb://savetv:aipapai@ds237669.mlab.com:37669/savetv-dev'
+    }
+
