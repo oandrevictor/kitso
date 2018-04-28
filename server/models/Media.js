3ad59328987777a58c4c8295f34820507e2b5aa2
Gabriel Morais <gabmla19@gmail.com>
2018-04-28 16:54:08 -0300

    Media model

--- server/models/Media.js
+++ server/models/Media.js
@@ -2,7 +2,35 @@ var mongoose = require('mongoose');
 var Schema = mongoose.Schema;
 
 var MediaSchema = new Schema({
-    // TO DO
+    name: {
+        type: String,
+        required: true
+    },
+    overview: {
+        type: String,
+        required: true,
+        default: ''
+    },
+    release_date: {
+        type: Date,
+        required: true
+    },
+    director: { type: mongoose.Schema.Types.ObjectId, ref: 'Person' },
+    actors: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Person' }],
+    imdb_id: {
+        type: String,
+        required: true
+    },
+    genres: [String],
+    porter: {
+        type: String,
+        default: 'placeholder'
+    },
+    rating: {
+        type: Number,
+        required: true,
+        default: 0
+    }
 });
 
 var Media = mongoose.model('Media', MediaSchema);
