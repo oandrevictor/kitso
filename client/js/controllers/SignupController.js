8e7418daec5ce4f385a1f7676f335557499dd37f
David <david.souza@ccc.ufcg.edu.br>
2018-04-30 18:28:28 -0300

    Adding validation to fields

--- client/js/controllers/SignupController.js
+++ client/js/controllers/SignupController.js
@@ -1,3 +1,31 @@
 angular.module('SignupController', []).controller('SignupController', function($scope) {
+    $scope.submitForm = function() {
+        this.checkPassword();
 
+        if ($scope.userForm.$valid) {
+            // SUBMITE REQUEST HERE
+            console.log('Submited');
+        }
+    };
+
+    $scope.isInvalid = function (field) {
+        return (field.$invalid && !field.$pristine);
+    };
+
+    $scope.checkPassword = function () {
+        var password = $scope.userForm.password.$modelValue;
+        var passwordConfirmation = $scope.userForm.passwordConfirmation.$modelValue;
+
+        if (password !== passwordConfirmation) {
+            $scope.userForm.password.$setValidity('password', false);
+            $scope.userForm.passwordConfirmation.$setValidity('passwordConfirmation', false);
+        }
+    };
+
+    $scope.resetValidity = function () {
+        if ($scope.userForm.$submitted) {
+            $scope.userForm.password.$setValidity('password', true);
+            $scope.userForm.passwordConfirmation.$setValidity('passwordConfirmation', true);
+        }
+    }
 });
