dd3e852c170dd9e51ab2e63a706f22c25adaf2a4
Rafaela9136 <rafaela9136@gmail.com>
2018-04-29 21:37:09 -0300

    Create + update user using bcrypt

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -1,4 +1,5 @@
-var User = require('../models/User');
+var User   = require('../models/User');
+var bcrypt = require('bcryptjs');
 
 // Todos usuários
 exports.index = function(req, res) {
@@ -26,12 +27,22 @@ exports.show = function(req, res) {
 exports.create = function(req, res) {
     var user = new User(req.body);
 
-    user.save()
-    .catch((err) => {
-        res.status(400).send(err);
-    })
-    .then((createdExample) => {
-        res.status(200).send('User created.');
+    bcrypt.hash(req.body.password, 10, function(err, hash) {
+        if (err) {
+            res.status(400).send(err);
+        } else {
+          user.password = hash;
+          user.save(function(err) {
+            if (err) {
+              if (err.name === 'MongoError' && err.code === 11000) {
+                return res.status(400).send('User already exists.');
+              }
+              return res.status(400).send(err);
+            } else {
+              res.status(200).send('User created.');
+            }
+          });
+        }
     });
 };
 
@@ -42,14 +53,37 @@ exports.update = function(req, res) {
         res.status(400).send(err);
     })
     .then((user) => {
-        user.nome = req.body.nome;
+        if (req.body.name) user.name = req.body.name;
+        if (req.body.username) user.username = req.body.username;
+        if (req.body.email) user.email = req.body.email;
+        if (req.body.birthday) user.birthday = req.body.birthday;
+        if (req.body.gender) user.gender = req.body.gender;
+        if (req.body._history) user._history = req.body._history;
+        if (req.body._following) user._following = req.body._following;
+        if (req.body._following_pages) user._following_pages = req.body._following_pages;
+        if (req.body._followers) user._followers = req.body._followers;
+        if (req.body.vip) user.vip = req.body.vip;
+        if (req.body._watchlist) user._watchlist = req.body._watchlist;
+        if (req.body._lists) user._lists = req.body._lists;
+        if (req.body._ratings) user._ratings = req.body._ratings;
 
-        user.save()
-        .catch((err) => {
-            res.status(400).send(err);
-        })
-        .then((updateUser) => {
-            res.status(200).json(updateUser);
+
+        bcrypt.hash(req.body.password, 10, function(err, hash) {
+            if (err) {
+                res.status(400).send(err);
+            } else {
+              user.password = hash;
+              user.save(function(err) {
+                if (err) {
+                  if (err.name === 'MongoError' && err.code === 11000) {
+                    return res.status(400).send('User already exists.');
+                  }
+                  return res.status(400).send(err);
+                } else {
+                  res.status(200).send('User updated.');
+                }
+              });
+            }
         });
     });
 };
