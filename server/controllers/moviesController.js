ba0040962a62608b597645d7f62aae20c058e043
Rafaela9136 <rafaela9136@gmail.com>
2018-05-05 17:17:11 -0300

    Fix movies update

--- server/controllers/moviesController.js
+++ server/controllers/moviesController.js
@@ -42,7 +42,15 @@ exports.update = function(req, res) {
         res.status(400).send(err);
     })
     .then((movie) => {
-        movie.nome = req.body.nome;
+        if (req.body.name) show.name = req.body.name;
+        if (req.body.overview) show.overview = req.body.overview;
+        if (req.body.release_date) show.release_date = req.body.release_date;
+        if (req.body._directors) show._directors = req.body._directors;
+        if (req.body._actors) show._actors = req.body._actors;
+        if (req.body.imdb_id) show.imdb_id = req.body.imdb_id;
+        if (req.body.genres) show.genres = req.body.genres;
+        if (req.body.poster) show.poster = req.body.poster;
+        if (req.body.isBoxOffice) show.isBoxOffice = req.body.isBoxOffice;
 
         movie.save()
         .catch((err) => {
