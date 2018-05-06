c856a9ede640467dde031a948b9fd6efaeb46e19
Gabriela Motta <gabriela.oliveira@ccc.ufcg.edu.br>
2018-05-06 18:42:46 -0300

    add profile settings tab

--- server/app.js
+++ server/app.js
@@ -72,6 +72,10 @@ app.get('/home', function (req, res) {
   res.sendfile(path.resolve('client/index.html'));
 });
 
+app.get('/profile', function (req, res) {
+  res.sendfile(path.resolve('client/index.html'));
+});
+
 // Api routes
 var exampleRoutes = require('./routes/example');
 app.use('/example', exampleRoutes);
