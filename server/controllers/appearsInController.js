8a798572c02c8aa05cecc36f8c377212d7537d76
Italo Batista <italo.batista@ccc.ufcg.edu.br>
2018-05-06 04:24:46 -0300

    Create AppearsIn controller for API

---
+++ server/controllers/appearsInController.js
@@ -0,0 +1,66 @@
+var AppearsIn = require('../models/AppearsIn');
+
+exports.index = function(req, res) {
+    AppearsIn.find({})
+    .catch((err) => {
+        res.status(400).send(err);
+    })
+    .then((result) => {
+        res.status(200).json(result);
+    });
+};
+
+exports.show = function(req, res) {
+    AppearsIn.findById(req.params.appearsin_id)
+    .catch((err) => {
+        res.status(400).send(err);
+    })
+    .then((result) => {
+        res.status(200).json(result);
+    });
+};
+
+exports.create = function(req, res) {
+    var appearsin = new AppearsIn(req.body);
+    appearsin.save()
+    .catch((err) => {
+        res.status(400).send(err);
+    })
+    .then((createdAppearsIn) => {
+        res_json = {
+            "message": "AppearsIn created",
+            "data": {
+                "appearsInId": createdAppearsIn._id,
+            }            
+        }
+        res.status(200).send(res_json);
+    });
+};
+
+exports.update = function(req, res) {
+    AppearsIn.findById(req.params.appearsin_id)
+    .catch((err) => {
+        res.status(400).send(err);
+    })
+    .then((appearsin) => {
+        appearsin._person = req.body._person;
+        appearsin._media = req.body._media;
+        appearsin.save()
+        .catch((err) => {
+            res.status(400).send(err);
+        })
+        .then((updateAppearsIn) => {
+            res.status(200).json(updateAppearsIn);
+        });
+    });
+};
+
+exports.delete = function(req, res) {
+    AppearsIn.remove({ _id: req.params.appearsin_id})
+    .catch((err) => {
+        res.status(400).send(err);
+    })
+    .then(() => {
+        res.status(200).send('AppearsIn removed.');
+    });
+};
