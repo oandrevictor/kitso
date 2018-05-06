6fc03c78758dcb885df816a4bc970e7441afd202
Gabriel Morais <gabmla19@gmail.com>
2018-05-05 23:36:56 -0300

    login fail messages

--- server/config/passport.js
+++ server/config/passport.js
@@ -37,11 +37,11 @@ module.exports = function(passport){
             }
             // if no user is found, return the message
             if (!user) {
-                return done(null, false); // req.flash is the way to set flashdata using connect-flash
+                return done(null, false, 'User not found.'); // req.flash is the way to set flashdata using connect-flash
             }
             // if the user is found but the password is wrong
             if (!user.validPassword(password)) {
-                return done(null, false); // create the loginMessage and save it to session as flashdata
+                return done(null, false, 'Wrong password.'); // create the loginMessage and save it to session as flashdata
             }
             // all is well, return successful user
             return done(null, user);
