17a18679b004ef580710e0f3d0f70088a8b269d5
André Lopes <oandrevictor@gmail.com>
2018-05-06 21:03:57 -0300

    heroku changes

--- server/config/db.js
+++ server/config/db.js
@@ -1,4 +1,4 @@
 // config/db.js
     module.exports = {
-        url : "mongodb://"+process.env.DATABASE_USERNAME+":"+process.env.DATABASE_PASSWORD+"@ds237669.mlab.com:37669/savetv-dev";
+        url : "mongodb://"+process.env.DATABASE_USERNAME+":"+process.env.DATABASE_PASSWORD+"@ds237669.mlab.com:37669/savetv-dev"
     }
