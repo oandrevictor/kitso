e27bb8ba7bf14948b5a052d287b64376cf950d2c
Italo Batista <italo.batista@ccc.ufcg.edu.br>
2018-05-13 11:28:24 -0300

    Resolving asycn bd actions

--- server/controllers/watchedController.js
+++ server/controllers/watchedController.js
@@ -4,13 +4,13 @@ var User = require('../models/User');
 
 const WATCHED_ACTION_TYPE = "watched";
 
-exports.create = function(req, res) {
+exports.create = async function(req, res) {
     var watched = new Watched(req.body);
     let user_id = watched._user;
-    let action = create_action(user_id, watched._id);
-    add_action_to_user_history(user_id, action._id);
+    let action = await create_action(user_id, watched._id);  
     watched._action = action;
-    watched.save()
+    await add_action_to_user_history(user_id, action._id);
+    watched.save()    
     .catch((err) => {
         res.status(400).send(err);
     })
@@ -19,21 +19,20 @@ exports.create = function(req, res) {
     });
 };
 
-var create_action = function(user_id, watched_id) {
+var create_action = async function(user_id, watched_id) {
     var action = new Action({
         _user: user_id,
         date: new Date(),
         _action: watched_id,
         action_type: WATCHED_ACTION_TYPE,
     });
-    action.save();
-    return action;
+    return action.save();
 }
 
-var add_action_to_user_history = function(user_id, action_id) {        
-    User.findById(user_id, function (err, user) {
+var add_action_to_user_history = async function(user_id, action_id) {        
+    User.findById(user_id, async function (err, user) {
         let user_history = user._history;
         user_history.push(action_id);
-        user.save();
+        return user.save();
     });
 }
