6803047161f557e7051ff209d5545c592b7a2c43
André Lopes <oandrevictor@gmail.com>
2018-05-15 17:24:40 -0300

    onlne

--- server/app.js
+++ server/app.js
@@ -21,7 +21,7 @@ var port = process.env.PORT || 8080;
 
 // connect to our mongoDB database
 // (uncomment after you enter in your own credentials in config/db.js)
-mongoose.connect(db.local_url);
+mongoose.connect(db.url);
 
 // Passport and sessions
 require('./config/passport')(passport);
