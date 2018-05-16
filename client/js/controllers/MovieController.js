38ad99a0fda24ef47ebc2e66a03032d1bd76271f
Gabriela Motta <gabriela.oliveira@ccc.ufcg.edu.br>
2018-05-16 19:04:43 -0300

    start rated view for movies

--- client/js/controllers/MovieController.js
+++ client/js/controllers/MovieController.js
@@ -1,8 +1,8 @@
 var kitso = angular.module('kitso');
 
 kitso.controller('MovieController',
-['$scope', '$location', '$timeout', 'MovieService', 'WatchedService', '$routeParams', 'AuthService',
-function($scope, $location, $timeout, MovieService, WatchedService, $routeParams, AuthService) {
+['$scope', '$location', '$timeout', 'MovieService', 'WatchedService', 'RatedService', '$routeParams', 'AuthService',
+function($scope, $location, $timeout, MovieService, WatchedService, RatedService, $routeParams, AuthService) {
 
     MovieService.loadMovie($routeParams.movie_id)
         .then(() => {
@@ -10,6 +10,17 @@ function($scope, $location, $timeout, MovieService, WatchedService, $routeParams
             $scope.user = AuthService.getUser();
             $scope.movie = MovieService.getMovie();
             $scope.release_date_formated = moment($scope.movie.release_date).format('YYYY');
+            RatedService.isRated($scope.user._id ,$routeParams.movie_id).then((rated) => {
+                $scope.movie.rated = rated;
+                if (! rated.rated_id)
+                  $scope.movie.rated = false;
+            }).catch((error) => {
+              UIkit.notification({
+                  message: '<span uk-icon=\'icon: check\'></span> ' + error.errmsg,
+                  status: 'danger',
+                  timeout: 2500
+              });
+            });
             WatchedService.isWatched($scope.user._id ,$routeParams.movie_id).then((watched) => {
                 $scope.movie.watched = watched;
                 if (! watched.watched_id)
@@ -21,6 +32,7 @@ function($scope, $location, $timeout, MovieService, WatchedService, $routeParams
                   timeout: 2500
               });
             });
+            
           }).catch(function(){
 
           })
@@ -65,5 +77,28 @@ function($scope, $location, $timeout, MovieService, WatchedService, $routeParams
   $scope.editionMode = function () {
     $location.path('movie/edit/' + $routeParams.movie_id);
   }
+
+  $scope.markAsRated = function(movieId, rating) {
+    RatedService.markAsRated($scope.user._id, movieId, date = moment(), rating)
+    .then((rated) => {
+        $scope.movie.rated = rated;
+    })
+    .catch((error) => {
+        UIkit.notification({
+            message: '<span uk-icon=\'icon: check\'></span> ' + error.errmsg,
+            status: 'danger',
+            timeout: 2500
+        });
+    });
+    }
+    
+
+    $scope.range = function(count){
+        var ratings = []; 
+        for (var i = 0; i < count; i++) { 
+            ratings.push(i+1) 
+        } 
+        return ratings;
+    }
 
 }]);
---
+++ client/js/services/rated.service.js
@@ -0,0 +1,124 @@
+var kitso = angular.module('kitso');
+
+kitso.service('RatedService', ['$q','$http', function ($q, $http) {
+
+    var rated = false;
+    var rateds = {};
+
+    // return available functions for use in the controllers
+    return ({
+      getAllRated: getAllRated,
+      markAsRated: markAsRated,
+      markAsNotRated: markAsNotRated,
+      isRated: isRated,
+      updateRated: updateRated
+    });
+
+    function getAllRated(userId){
+      var deferred = $q.defer();
+      $http.get('/api/rated/user/' + userId)
+          .then((response) => {
+            if (response.status === 200) {
+                  deferred.resolve(response.data);
+              } else {
+                  deferred.reject();
+              }
+          })
+          .catch((error) => {
+              deferred.reject(error.data);
+          });
+
+      return deferred.promise;
+    }
+
+
+    function markAsRated(userId, mediaId, date = moment(), rating) {
+        var deferred = $q.defer();
+
+        var data = {
+            "_user": userId,
+            "_media": mediaId,
+            "date" : date,
+            "rating" : rating
+        };
+
+        $http.post('/api/rated/', data)
+            .then((response) => {
+                if (response.status === 200) {
+                  response.data.rated_id = response.data._id
+                    deferred.resolve(response.data);
+                } else {
+                    deferred.reject();
+                }
+            })
+            .catch((error) => {
+                deferred.reject(error.data);
+            });
+
+        return deferred.promise;
+    }
+
+
+    function updateRated(rated) {
+        var deferred = $q.defer();
+        var date = rated.date;
+        var rating = rated.rating;
+
+        var data = {
+            "date" : date,
+            "rating" : rating
+        };
+
+        $http.put('/api/rated/' + rated._id, data)
+            .then((response) => {
+                if (response.status === 200) {
+                    deferred.resolve(response.data);
+                } else {
+                    deferred.reject();
+                }
+            })
+            .catch((error) => {
+                deferred.reject(error.data);
+            });
+
+        return deferred.promise;
+    }
+
+
+    function markAsNotRated(ratedId) {
+        var deferred = $q.defer();
+
+        $http.delete('/api/rated/' + ratedId)
+            .then((response) => {
+                if (response.status === 200) {
+                    deferred.resolve();
+                } else {
+                    deferred.reject();
+                }
+            })
+            .catch((error) => {
+                deferred.reject(error.data);
+            });
+
+        return deferred.promise;
+    }
+
+    function isRated(userId, mediaId) {
+        var deferred = $q.defer();
+        $http.get('/api/rated/is_rated?user_id='+ userId + "&media_id="+mediaId)
+            .then((response) => {
+                if (response.status === 200) {
+                    rated = response.data.is_rated;
+                    deferred.resolve(response.data);
+                } else {
+                    deferred.reject();
+                }
+            })
+            .catch((error) => {
+                deferred.reject(error.data);
+            });
+
+        return deferred.promise;
+    }
+
+}]);
