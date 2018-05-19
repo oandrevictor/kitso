7a57a6a672fb3b2df219e59a52c6887ecca2cf41
Gabriel Morais <gabmla19@gmail.com>
2018-05-19 15:10:33 -0300

    Revert "Crud news"

--- server/models/Related.js
+++ server/models/Related.js
@@ -2,6 +2,11 @@ var mongoose = require('mongoose');
 var Schema = mongoose.Schema;
 
 var RelatedSchema = new Schema({
+    _user: {
+      type: mongoose.Schema.Types.ObjectId, 
+      ref: 'User',
+      required: true
+    },
     _news: {
       type: mongoose.Schema.Types.ObjectId, 
       ref: 'News',
