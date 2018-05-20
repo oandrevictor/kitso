eaa56c3411b7ed52a6a478de51ec6707398aa58c
Rafaela9136 <rafaela9136@gmail.com>
2018-05-20 19:53:42 -0300

    following me pages

--- server/routes/followsPage.js
+++ server/routes/followsPage.js
@@ -8,6 +8,9 @@ router.get('/user/:user_id', followsPageController.index);
 //is_following?user_id=user_id&following_id=following_id
 router.get('/is_following', followsPageController.is_following);
 
+//following_me?page_id=page_id
+router.get('/following_me', followsPageController.following_me);
+
 router.post('/', followsPageController.create);
 
 router.delete('/:followsPage_id', followsPageController.delete);
