be657e4cd3503d37975360bd8e07093e4b23705e
Italo Batista <italo.batista@ccc.ufcg.edu.br>
2018-05-28 20:44:22 -0300

    Add more util functions

--- server/utils/lib/dataStoreUtils.js
+++ server/utils/lib/dataStoreUtils.js
@@ -1,40 +1,47 @@
 
 var AppearsIn = require('../../models/AppearsIn');
 var Person = require('../../models/Person');
-var Media = require('../../models/Media');
 var Utils = require('./utils')
+var Media = require('../../models/Media');
+
+exports.getMediaObjById = function(mediaId) {
+    return Media.findById(mediaId).exec();
+}
 
 exports.getPersonObjById = async function(personId) {
     return Person.findById(personId).exec();
 }
 
-exports.getMediaEntityById = function(mediaId) {
-    return Movie.find({_id: mediaId}).exec();
+exports.getAppearsInObjById = function(appearsInId) {
+    return AppearsIn.findById(appearsInId).exec();
 }
-exports.getAppearsInByKeys = async function(mediaId, personId) {
+
+exports.getAppearsInObjByKeys = async function(mediaId, personId) {
     let array = await AppearsIn.find({_media: mediaId, _person: personId}).exec();
-    console.log('appears array')
-    console.log(array);
     return array[0];  // since there's just 1 appearsIn with same mediaid and personId
 }
 
 exports.removeMediaFromPerson = async function(mediaId, personId) {    
     let person = await this.getPersonObjById(personId);
-    console.log('person')
-    console.log(person)
-    let appearsIn = await this.getAppearsInByKeys(mediaId, personId);
-    console.log('appears')    
-    console.log(appearsIn)
+    let appearsIn = await this.getAppearsInObjByKeys(mediaId, personId);
     let appearsInId = appearsIn._id;
     Utils.removeItemFromList(appearsInId, person._appears_in);
     person.save();
 }
 
 exports.deleteAppearsInByKeys = async function(mediaId, personId) {
-    let appearsIn = await this.getAppearsInByKeys(mediaId, personId);
+    let appearsIn = await this.getAppearsInObjByKeys(mediaId, personId);
     appearsIn.remove();
 }
 
-exports.test = function() {
-    console.log('deu certo')
+exports.deleteMediaById = async function(mediaId) {    
+    let media = await this.getMediaObjById(mediaId);
+    let actors = media._actors;
+    await actors.forEach(async (personId) => {
+        // deleting media from actors' appearsins
+        await this.removeMediaFromPerson(mediaId, personId);
+        // deleting appearsIns entities with deleted media
+        await this.deleteAppearsInByKeys(mediaId, personId);        
+    });
+    media.remove();
 }
