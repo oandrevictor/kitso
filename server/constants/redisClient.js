20402180fd0888f07df1e6bb85d8326af234b65f
Italo Batista <italo.batista@ccc.ufcg.edu.br>
2018-06-02 17:07:33 -0300

    Constants for redisClient number magics params

--- server/constants/redisClient.js
+++ server/constants/redisClient.js
@@ -1,9 +1,24 @@
 const ACCESS_TOKEN = "nsXmMM8VvJ7PrbYc4q6WZ50ilryBdbmM";
+const PORT = 19990;
+const SERVER = 'redis-19990.c16.us-east-1-2.ec2.cloud.redislabs.com';
+const OPTIONS = {no_ready_check: true};
 
 class RedisClientConstants {
 
     static get ACCESS_TOKEN() {
         return ACCESS_TOKEN;
+    }
+
+    static get PORT() {
+        return PORT;
+    }
+
+    static get SERVER() {
+        return SERVER;
+    }
+
+    static get OPTIONS() {
+        return OPTIONS;
     }
 
 }
--- server/utils/lib/redisClient.js
+++ server/utils/lib/redisClient.js
@@ -3,9 +3,9 @@ var RedisClientConstants = require('../../constants/redisClient');
 
 exports.createAndAuthClient = function() {
     let client = redis.createClient(
-        19990,
-        'redis-19990.c16.us-east-1-2.ec2.cloud.redislabs.com',
-        {no_ready_check: true});
+        RedisClientConstants.PORT,
+        RedisClientConstants.SERVER,
+        RedisClientConstants.OPTIONS);
 
     client.auth(RedisClientConstants.ACCESS_TOKEN, function (err) {
         if (err) throw err;
