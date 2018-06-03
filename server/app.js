6135d9ca983d114f500bc4bdf9bd459e093b0f81
Italo Batista <italo.batista@ccc.ufcg.edu.br>
2018-06-02 21:01:21 -0300

    Fix Redis connection already closed Error

--- server/app.js
+++ server/app.js
@@ -11,14 +11,9 @@ var passport       = require('passport');
 var session        = require('express-session');
 var MongoStore     = require('connect-mongo')(session);
 var dotenv         = require('dotenv').load();
-// var redis = require('redis');
 var RedisClient = require('./utils/lib/redisClient');
 
 let client = RedisClient.createAndAuthClient();
-//var client = redis.createClient(19990, 'redis-19990.c16.us-east-1-2.ec2.cloud.redislabs.com', {no_ready_check: true});
-// client.auth('nsXmMM8VvJ7PrbYc4q6WZ50ilryBdbmM', function (err) {
-//     if (err) throw err;
-// });
 
 // configuration ===========================================
 
