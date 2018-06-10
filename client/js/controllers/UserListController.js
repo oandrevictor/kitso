be57e4b6fdc16493ab7c036134700a64fd4311f6
David <david.souza@ccc.ufcg.edu.br>
2018-06-09 22:57:55 -0300

    Adding remove option to userlist page

--- client/js/controllers/UserListController.js
+++ client/js/controllers/UserListController.js
@@ -3,16 +3,30 @@ var kitso = angular.module('kitso');
 kitso.controller('UserListController',
 ['$scope', '$location', '$timeout', 'UserListService', 'MovieService','$routeParams', 'AuthService',
 function($scope, $location, $timeout, UserListService, MovieService, $routeParams, AuthService) {
-  UserListService.loadUserList($routeParams.userlist_id).then(() => {
-    $scope.userlist = UserListService.getUserList();
-    loadUserListBackground();
-  }).catch((error) => {
+  AuthService.getStatus().then(function () {
+    $scope.user = AuthService.getUser();
+    loadUserList();
+  })
+    .catch((error) => {
       UIkit.notification({
-          message: '<span uk-icon=\'icon: check\'></span> ' + error.errmsg,
-          status: 'danger',
-          timeout: 2500
+        message: '<span uk-icon=\'icon: check\'></span> ' + error.errmsg,
+        status: 'danger',
+        timeout: 2500
       });
-  });
+    });
+
+  var loadUserList = function () {
+    UserListService.loadUserList($routeParams.userlist_id).then(() => {
+      $scope.userlist = UserListService.getUserList();
+      loadUserListBackground();
+    }).catch((error) => {
+      UIkit.notification({
+        message: '<span uk-icon=\'icon: check\'></span> ' + error.errmsg,
+        status: 'danger',
+        timeout: 2500
+      });
+    });
+  };
 
   $scope.goToMedia = function (media) {
     if (media.__t === 'TvShow') {
@@ -27,6 +41,26 @@ function($scope, $location, $timeout, UserListService, MovieService, $routeParam
 
   $scope.getPoster = function(media){
     return 'https://image.tmdb.org/t/p/w500/' + media.poster_path;
+  }
+
+  $scope.removeFromList = function(userListId, ranked) {
+    console.log('poc');
+    UserListService.deleteItem(userListId, $scope.user._id, ranked)
+      .then((deleted) => {
+        loadUserList();
+        UIkit.notification({
+          message: '<span uk-icon=\'icon: check\'></span> Removed!',
+          status: 'success',
+          timeout: 1500
+        });
+      })
+      .catch((error) => {
+        UIkit.notification({
+          message: '<span uk-icon=\'icon: check\'></span> ' + error.errmsg,
+          status: 'danger',
+          timeout: 2500
+        });
+      });
   }
 
   var loadUserListBackground = function () {
