24c9d1a2d8ffe92f4e57423d63a146e05c621c55
David <david.souza@ccc.ufcg.edu.br>
2018-06-15 21:17:06 -0300

    Fix slider - adding pagination and color background to slide navigation

--- client/js/controllers/MovieController.js
+++ client/js/controllers/MovieController.js
@@ -95,6 +95,7 @@ function($scope, $location, $timeout, MovieService, WatchedService, FollowServic
         });
 
 
+
     $scope.addToList = function(movieId, userListId){
         UserListService.addItem(userListId, movieId, $scope.user._id, date = moment())
         .then((added) => {
@@ -301,5 +302,4 @@ function($scope, $location, $timeout, MovieService, WatchedService, FollowServic
         }
         return ratings;
     }
-
 }]);
