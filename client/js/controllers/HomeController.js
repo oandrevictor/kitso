a95c263fbfba9bd035dba85abb436f2bf4371ca4
André Lopes <oandrevictor@gmail.com>
2018-06-19 00:47:41 -0300

    Show activity + static actions

--- client/js/controllers/HomeController.js
+++ client/js/controllers/HomeController.js
@@ -1,6 +1,6 @@
 var kitso = angular.module('kitso');
 
-kitso.controller('HomeController', ['$scope', '$location', '$timeout', 'AuthService', function($scope, $location, $timeout, AuthService) {
+kitso.controller('HomeController', ['$scope', '$location', '$timeout', 'AuthService', 'FeedService', function($scope, $location, $timeout, AuthService, FeedService) {
 
 	$scope.logout = function() {
 		AuthService.logout()
@@ -28,8 +28,120 @@ kitso.controller('HomeController', ['$scope', '$location', '$timeout', 'AuthServ
                     });
                 });
 	};
+	var compareDates = function(a,b){
+    return - moment(a.date).diff(moment(b.date))
+  }
+
+	AuthService.getStatus().then(function(){
+    $scope.user = AuthService.getUser();
+		loadFeed($scope.user._id);
+
+  }).catch(function(){});
+
+	var loadFeed = function(userId){
+		FeedService.getFollowingUsersActivity(userId).then(function(result){
+			$scope.feed = result;
+
+			$scope.feed.forEach(function(activity){
+				if(['watched', 'rated'].includes(activity.action_type)){
+					activity.open = true;
+				}
+			})
+		$scope.feed = $scope.feed.sort(compareDates)
+		})
+		.catch(
+			function(result){
+				console.log(result)
+			}
+		)
+	}
+
+
 
 	$scope.isLogged = function() {
 		return AuthService.isLogged();
+	};
+
+	$scope.getActivityType = function(activity){
+		if(activity.action_type == 'followed-page') {
+			return 'followed';
+		}
+		else {
+			return activity.action_type;
+		}
 	}
+
+	$scope.getSeasonInfo = function(activity){
+		episode = activity._action._media;
+		seasons = episode.show.seasons;
+		season_number = episode.season_number - 1;
+		return seasons[season_number]
+	}
+
+	$scope.getActivityImage = function(activity){
+		if(["watched","rated"].includes(activity.action_type)){
+			if(activity._action._media){
+				if (!activity._action._media.backdrop_path){
+					return "https://image.tmdb.org/t/p/original/" + activity._action._media.still_path;
+				}
+				else {
+					return activity._action._media.backdrop_path;
+				}
+			}
+		}
+	}
+
+	$scope.getActivityReferedObject = function(activity){
+		switch(activity.action_type) {
+    case 'rated':
+			if (!activity._action._media.name )
+				activity._action._media.name = activity._action._media.title;
+    	return activity._action._media;
+  		break;
+		case 'watched':
+			if (!activity._action._media.name )
+				activity._action._media.name = activity._action._media.title;
+	    return activity._action._media;
+	  	break;
+		case 'followed':
+	    return activity._action._following;
+	  	break;
+		case 'followed-page':
+			if (!activity._action._following.name)
+				activity._action._following.name = activity._action._following.title;
+	    return activity._action._following;
+	  	break;
+    default:
+    	return 'a'
+		}
+	}
+
+	$scope.getActivityDate = function(activity){
+		then = moment(new Date(activity.date));
+		now = moment();
+		mins_dif = now.diff(then, 'minutes');
+		hours_dif = now.diff(then, 'hours');
+		days_dif = now.diff(then, 'days');
+		months_dif = now.diff(then, 'months')
+		years_dif = now.diff(then, 'years')
+		if (mins_dif < 1)
+			return "just now"
+		if (years_dif >= 1)
+			return years_dif.toString() + " years ago"
+		if (months_dif >= 1)
+			return months_dif.toString() + " months ago"
+		if (days_dif >= 1)
+			return days_dif.toString() + " days ago"
+		if (hours_dif >= 1)
+			return hours_dif.toString() + " hours ago"
+		else
+			return mins_dif.toString() + " minutes ago"
+
+	}
+
+	$scope.getActivityUser = function(activity){
+		return activity._user;
+	}
+
+
 }]);
--- server/external/TMDBController.js
+++ server/external/TMDBController.js
@@ -3,6 +3,7 @@ var RequestGenerals = require('../constants/requestGenerals');
 var RedisClient = require('../utils/lib/redisClient');
 var Show = require('../models/TvShow');
 var Movie = require('../models/Movie');
+var Episode = require('../models/Episode');
 
 
 const https = require('https');
@@ -132,6 +133,40 @@ exports.getMovie = function(tmdb_id) {
     })
   });
 };
+
+exports.getEpisode = function(tmdb_id){
+  return new Promise(function(resolve, reject) {
+    console.log("Get episode:" + tmdb_id)
+    Episode.findOne({_tmdb_id: tmdb_id})
+    .catch((err)=> console.log(err))
+    .then(async (result)=>{
+      var query = RequestGenerals.MOVIE_ENDPOINT + tmdb_id;
+      redisClient.exists(query, function(err, reply) {
+        if (reply === 1) {
+          console.log("got query from redis: " + query)
+          redisClient.get(query, async function(err,data) {
+            if (err)
+            console.log(err);
+            else {
+              var parsed_result = JSON.parse(JSON.parse(data));
+              parsed_result.backdrop_path = TMDBConstants.TMDB_BACK_IMAGE_PATH + parsed_result.still_path;
+              resolve(parsed_result);
+            }
+          });
+        } else {
+          console.log(result)
+          exports.getEpisodeFromTMDB(result._tmdb_tvshow_id, result.season_number, result.number).then(async function(data) {
+            var data = JSON.parse(data);
+            data._id = result._id;
+            data.__t = result.__t;
+            resolve(data);
+          })
+        }
+      })
+    })
+  })
+
+}
 
 exports.getMovieFromTMDB = function(tmdb_id){
   return new Promise(function(resolve, reject) {
