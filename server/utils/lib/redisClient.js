6c772cb80d2bbb2e9d86be0330344aeaa261a9b3
André Lopes <oandrevictor@gmail.com>
2018-07-01 19:12:30 -0300

    ENVS

--- server/utils/lib/redisClient.js
+++ server/utils/lib/redisClient.js
@@ -2,21 +2,28 @@ var redis = require('redis');
 var RedisClientConstants = require('../../constants/redisClient');
 
 let redisClient = null;
+var ENV = process.env.ENVIROMENT || 'development'
 
 exports.createAndAuthClient = function() {
 
   if (redisClient)
     return redisClient;
 
-  redisClient = redis.createClient(
-    RedisClientConstants.PORT,
-    RedisClientConstants.SERVER,
-    RedisClientConstants.OPTIONS
-  );
-  
-  redisClient.auth(RedisClientConstants.ACCESS_TOKEN, function (err) {
-    if (err) throw err;
-  });
+  if(ENV == 'production'){
+    redisClient = redis.createClient(
+      RedisClientConstants.PORT,
+      RedisClientConstants.SERVER,
+      RedisClientConstants.OPTIONS
+    );
+    redisClient.auth(RedisClientConstants.ACCESS_TOKEN, function (err) {
+      if (err) throw err;
+    });
+  }
+  else {
+    redisClient = redis.createClient();
+  }
+
+
 
   return redisClient;
 };
